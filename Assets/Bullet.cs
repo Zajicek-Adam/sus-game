39ef011ce76bba627ba5a6800b963cbee0c974bf
Zeidash <adam.zajicek.020@pslib.cz>
2021-11-11 21:39:12 +0100

    Susman throws beans now
    Lmap

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float timer = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!col.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,15 @@ public class Enemy : MonoBehaviour
 {
     public Transform player;
     public Camera cam;
-    private Vector3 pos;
+    public float range;
+    public GameObject bean;
+    public Transform firePoint;
+    private bool shouldShoot = false;
+    private float firerate = 2.5f;
+    private Vector3 pos; // Position in the camera viewport
+    private Vector3 originalPosition; // Position before subtracting the Range
     private float startingY;
+    private Vector3 bulletVelocity = Vector3.zero;
     private Vector3 velocity = Vector3.zero;
     // Start is called before the first frame update
     void Start()
@@ -19,21 +26,58 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         pos = cam.WorldToViewportPoint(transform.position);
-        if(pos.z > 0) // Is in viewport
+        originalPosition = transform.position;
+        if (pos.z > 0 && pos.x > 0 && pos.y > 0) // Is in viewport
         {
             Attack();
-            Turn();
         }
+        Turn();
     }
     void Attack()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.position.x, startingY, player.position.z), ref velocity, 0.25f, 150f * Time.fixedDeltaTime);
+        if(transform.position.x - player.position.x > 0 && player.position.x - transform.position.x < 8)
+        {
+            range = 8f;
+        }
+        else if(transform.position.x - player.position.x < 0 && transform.position.x - player.position.x < 8)
+        {
+            range = -8f;
+        }
+        if (Mathf.Abs(transform.position.x - player.position.x) < 8)
+        {
+            shouldShoot = true;
+        }
+        else
+        {
+            shouldShoot = false;
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.position.x + range, startingY, player.position.z), ref velocity, 0.25f, 150f * Time.fixedDeltaTime);
+            shouldShoot = true;
+        }
+
+        if (shouldShoot)
+        {
+            firerate -= Time.deltaTime;
+            Vector2 offset = new Vector2(Random.Range(0f,1f), Random.Range(0f, 1f));
+            if (firerate <= 0)
+            {
+                firerate = 2.5f;
+                GameObject instantiatedBean;
+                instantiatedBean = Instantiate(bean);
+                instantiatedBean.transform.position = firePoint.position;
+                instantiatedBean.SetActive(true);
+                instantiatedBean.GetComponent<Rigidbody2D>().velocity = (player.position - instantiatedBean.transform.position).normalized * 400f * Time.fixedDeltaTime;
+            }
+        }
     }
     void Turn()
     {
-        if(player.position.x - transform.position.x < 0)
+        if (player.position.x - originalPosition.x < 0)
         {
             transform.rotation = new Quaternion(0, 180f, 0, 0);
+        }
+        else
+        {
+            transform.rotation = new Quaternion(0, 0f, 0, 0);
         }
     }
 }
