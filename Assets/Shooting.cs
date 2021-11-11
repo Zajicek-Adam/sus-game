475c05634b2047f09af4398c0681eed7b072729b
Zeidash <adam.zajicek.020@pslib.cz>
2021-11-11 23:11:43 +0100

    Raycasts

---
+++ Assets/Shooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public LineRenderer lineRenderer;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, mousePos - new Vector2(firePoint.position.x, firePoint.position.y), 2000f);
+        if (hit)
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hit.point);
+        }
+    }
+}
